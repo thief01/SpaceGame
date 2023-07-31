e216f4414c8dd6e3aa9fc5fc95625f59a7f01fd0
thief01 <pst12345@wp.pl>
2023-08-01 00:39:28 +0200

    add dead controller

---
+++ Assets/Scripts/Game/Character/DeadController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadController : MonoBehaviour
+{
+    [SerializeField] private GameObject spawnFX;
+    [SerializeField] private float fxAliveTime;
+
+
+    public void OnDie()
+    {
+        if (spawnFX != null)
+        {
+            GameObject g = Instantiate(spawnFX);
+            g.transform.position = transform.position;
+            Destroy(g, fxAliveTime);
+        }       
+
+        Destroy(gameObject);
+    }
+
+}

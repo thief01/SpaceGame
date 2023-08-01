49b238570a95984817117698ae45879f3ba2193b
thief01 <pst12345@wp.pl>
2023-08-02 00:09:48 +0200

    add multiplayer objects

---
+++ Assets/Scripts/Multiplayer/PhotonGameObject.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PhotonGameObject : MonoBehaviourPun, IPunObservable
+{
+    private PhotonView photonView;
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    private void OnEnable()
+    {
+        if (photonView.IsMine)
+        {
+            photonView.RPC("SwitchEnableObject", RpcTarget.OthersBuffered, true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (photonView.IsMine)
+        {
+            photonView.RPC("SwitchEnableObject", RpcTarget.OthersBuffered, false);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(gameObject.activeSelf);
+        }
+        else
+        {
+            var active = (bool)stream.ReceiveNext();
+            gameObject.SetActive(active);
+            
+        }
+    }
+
+    [PunRPC]
+    public void SwitchEnableObject(bool active)
+    {
+        gameObject.SetActive(active);
+    }
+}

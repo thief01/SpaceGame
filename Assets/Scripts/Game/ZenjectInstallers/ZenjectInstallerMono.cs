45244667699d88baece5c100c56d5e5fdf5ca1e0
thief01 <pst12345@wp.pl>
2023-08-02 00:30:39 +0200

    bind pools in zenject

--- Assets/Scripts/Game/ZenjectInstallers/ZenjectInstallerMono.cs
+++ Assets/Scripts/Game/ZenjectInstallers/ZenjectInstallerMono.cs
@@ -1,14 +1,35 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
 
 public class ZenjectInstallerMono : MonoInstaller<ZenjectInstallerMono>
 {
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject ship;
+    [SerializeField] private GameObject asteroid;
+    [SerializeField] private GameObject explosion;
+
+    [SerializeField] private List<ScriptableObject> scriptableObjectsToInject;
+
     public override void InstallBindings()
     {
         Debug.Log("Installing bindings");
         Container.Bind<IDamageProvider>().To<BaseDamageProvider>().FromInstance(new BaseDamageProvider());
-        Container.Bind<PlayerData>().AsSingle();
+
+        Container.Bind<BasePool>().WithId("Bullets").FromInstance(new SimplePool(bullet, 200));
+        Container.Bind<BasePool>().WithId("Ships").FromInstance(new SimplePool(ship, 1));
+        Container.Bind<BasePool>().WithId("Asteroids").FromInstance(new SimplePool(asteroid, 25));
+        Container.Bind<BasePool>().WithId("Explosions").FromInstance(new SimplePool(explosion, 25));
+        InjectScriptables();
+    }
+
+    private void InjectScriptables()
+    {
+        for (int i = 0; i < scriptableObjectsToInject.Count; i++)
+        {
+            Container.QueueForInject(scriptableObjectsToInject[i]);
+        }
     }
 }
